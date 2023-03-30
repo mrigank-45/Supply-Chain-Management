cef2bea2fae0d329d7c292853f0a10784ae2777b
mrigank-45 <mrigankshukla2015@gmail.com>
2023-03-30 21:26:01 +0530

    first commit

---
+++ contracts/Ownable.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+// pragma solidity ^0.6.0;
+pragma solidity 0.8.14;
+
+contract Ownable {
+    address public _owner;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function isOwner() public view returns (bool) {
+        return (msg.sender == _owner);
+    }
+}
